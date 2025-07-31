89c554aaad558006e65e86d1ca72736425c78cf4
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-31 13:13:48 +0300

    add soft delete flag and enable timestamps in note schema

--- src/models/note.model.ts
+++ src/models/note.model.ts
@@ -4,6 +4,7 @@ export interface INote extends Document {
   title: string;
   content?: string;
   tags?: string[];
+  isDeleted: boolean;
   createdAt: Date;
   updatedAt: Date;
 }
@@ -13,9 +14,10 @@ const noteSchema = new Schema<INote>(
     title: { type: String, required: true },
     content: { type: String },
     tags: [{ type: String }],
+    isDeleted: { type: Boolean, default: false },
   },
   {
-    timestamps: true, // Automatically adds createdAt and updatedAt
+    timestamps: true,
   }
 );
 
