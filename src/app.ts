13d2841c5b87aa23e4b1c422b37ec6a1efcaf6ac
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-31 12:56:52 +0300

    Initialize note routes and global error handlers

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,20 @@
 import express from 'express';
+import noteRoutes from './routes/note.routes';
+import errorHandler from './middlewares/errorHandler';
+import notFound from './middlewares/notFound';
 
 const app = express();
+
+// Middlewares
+app.use(express.json());
+
+// Routes
+app.use('/api/notes', noteRoutes);
+
+// 404 handler
+app.use(notFound);
+
+// Error handler
+app.use(errorHandler);
 
 export default app;
